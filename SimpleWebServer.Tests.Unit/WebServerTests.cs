07f91f5bc651135c4c3ea361e4127fcfdb5c7e08
Vadym <quadropoison@gmail.com>
2017-04-28 15:52:56 +0300

    Initialize Unit Tests project

---
+++ SimpleWebServer.Tests.Unit/WebServerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleWebServer.Tests.Unit
+{
+    [TestClass]
+    public class WebServerTests
+    {
+        private string PageAddress { get; } = "http://localhost:8090/index/";
+
+        private string SimpleHtmlCode { get; } = $"<HTML><BODY>Simple web page.<br><p>simple test</p></BODY></HTML>";
+
+        [TestMethod]
+        public void CallWebPage_ShouldReturn_ExpectedHtml()
+        {
+            var expectedHtml = SimpleHtmlCode;
+            var webServer = InitializeWebServer();
+
+            var actualHtml = GetActualHtml();
+            webServer.Stop();
+
+            Assert.IsTrue(expectedHtml == actualHtml);            
+        }
+
+        private string GetActualHtml()
+        {
+            WebClient client = new WebClient();
+            string actualHtml = client.DownloadString(PageAddress);
+            return actualHtml;
+        }
+
+        private WebServer InitializeWebServer()
+        {
+            WebServer webServer = new WebServer(SendResponse, PageAddress);
+            webServer.Run();
+            return webServer;
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return $"<HTML><BODY>Simple web page.<br><p>simple test</p></BODY></HTML>";
+        }
+    }
+}
